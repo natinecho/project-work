caf2de71435c79c46c2e245ac76ee153233afec6
RuthTadesse <125727910+RuthTadesse@users.noreply.github.com>
2023-04-09 18:31:58 +0300

    Update part 1 question 5.cpp

--- part 1 question 5.cpp
+++ part 1 question 5.cpp
@@ -32,5 +32,64 @@ int main() {
  // End of program
 return 0;
 }
+//using pointers for this problem
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    double loanAmount, interestRate, monthlyPayment, monthlyInterest, balance, totalInterest = 0.0;
+    int months = 0;
+    double *pMonthlyPayment = &monthlyPayment;
+    double *pMonthlyInterest = &monthlyInterest;
+    double *pBalance = &balance;
+
+    cout << "Enter loan amount: ";
+    cin >> loanAmount;
+
+    cout << "Enter interest rate (in percentage): ";
+    cin >> interestRate;
+
+    // Convert interest rate to decimal
+    interestRate /= 100.0;
+
+    // Calculate monthly payment (1/20th of loan amount)
+    monthlyPayment = loanAmount / 20.0;
+
+    cout << fixed << setprecision(2);
+    cout << "Monthly payment: $" << *pMonthlyPayment << endl;
+
+    balance = loanAmount;
+
+    // Output the table header
+    cout << "Month" << setw(15) << "Balance" << setw(18) << "Interest" << setw(16) << "Payment\n";
+
+    while (balance >= monthlyPayment) {
+        // Calculate monthly interest payment
+        monthlyInterest = (balance * interestRate) / 12.0;
+        // Calculate the payment towards the principal amount
+        double paymentTowardPrincipal = monthlyPayment - monthlyInterest;
+        // Calculate the new balance amount
+        balance -= paymentTowardPrincipal;
+
+        // Output the table values
+        cout << setw(4) << months + 1 << setw(15) << *pBalance << setw(18) << *pMonthlyInterest << setw(16) << *pMonthlyPayment << endl;
+        // Keep track of total interest paid
+        totalInterest += monthlyInterest;
+
+        months++;
+    }
+
+    // Output final balance and total interest paid
+    cout << "Final balance: $" << *pBalance << endl;
+    cout << "Total interest paid: $" << totalInterest << endl;
+
+    // Calculate simple annualized percentage paid in interest
+    double percentInterestPaid = (totalInterest / loanAmount) * 100.0;
+    cout << "Percent of original loan paid in interest: " << percentInterestPaid << "%" << endl;
+
+    return 0;
+}
+
 
 
