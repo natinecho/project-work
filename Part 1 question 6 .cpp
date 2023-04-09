56e8e257b663e901f874d1307f798e20855fe1c6
Olman <122857604+simera7@users.noreply.github.com>
2023-04-09 17:05:17 +0300

    Update Part 1 question 6 .cpp

--- Part 1 question 6 .cpp
+++ Part 1 question 6 .cpp
@@ -4,6 +4,29 @@ using namespace std;
 int main() {
   //declare variables
   menu:
+cout<<"**********************************"<<endl;
+cout<<"*  Temperature Adjustment Menu   *"<<endl;
+cout<<"**********************************"<<endl;
+cout<<endl;
+int to_input[4][3]={
+     {1,2,3},
+     {4,5,6},
+     {7,8,9},
+     {0}
+ };
+//for loop to display the numbers to the screen for user
+ for(int i=0; i<4; i++){
+
+    for(int j=0; j<3; j++){
+        if(i==3){
+            cout<<"\t"<<to_input[i][j]<<endl;
+            break;
+        }
+        cout<<to_input[i][j]<<"\t";
+    }
+    cout<<endl<<endl;
+ }
+
   int temperature,desired_max(0),desired_min(0);
   int *p=&temperature;
   int *max=&desired_max;
@@ -25,7 +48,8 @@ int main() {
     int m=pow(10,n);
  // Get the most significant digit of the temperature
     int num=*p/m;
-  // If the most significant digit is 1, 4, or 7, calculate desired max and min temperatures and exit loop
+  /* If the most significant digit is 1, 4, or 7, calculate 
+   desired max and min temperatures and exit loop*/
     if(num==1||num==4||num==7){
     *max=*max+ (num+1)*m ;
     *min=*min + ((num*m)-1);
@@ -53,6 +77,7 @@ char ask;
 cin>>ask;
 if(ask=='y'){
 goto menu;
+cout<<endl;
 cout<<endl;
 }
 //End of program
