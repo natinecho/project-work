fe973826c205bfee4cc154b37120848764268e29
RuthZinabu <130273152+RuthZinabu@users.noreply.github.com>
2023-04-09 15:25:06 +0300

    Update Part 1 question 6 .cpp

--- Part 1 question 6 .cpp
+++ Part 1 question 6 .cpp
@@ -3,34 +3,37 @@
 using namespace std;
 int main() {
   int temperature,desired_max(0),desired_min(0);
+int *p=&temperature;
+int *max=&desired_main;
+int *min=&desired_maun;
   lebl:
   cout<<"enter the temperature:";
   cin>>temperature;
-  if(temperature>999 || temperature<0){
+  if(*p>999 || *p<0){
     cout<<"the temprature is not in range try again!";
     goto lebl;
   }
-  while(temperature!=0){
-    int n=log10(temperature);
+  while(*p!=0){
+    int n=log10(*p);
     int m=pow(10,n);
-    int num=temperature/m;
+    int num=*p/m;
     if(num==1||num==4||num==7){
-    desired_max=desired_max+ (num+1)*m ;
-    desired_min=desired_min + ((num*m)-1);
+    *max=*max+ (num+1)*m ;
+    *min=*min + ((num*m)-1);
     break;
     }
     else{
-      desired_max=desired_max+ num*m;
-      desired_min=desired_min + num*m;
+      *max=*max+ num*m;
+      *min=*min + num*m;
     }
-    temperature%=m;
+    *p%=m;
   }
-  if(desired_max==desired_min){
-    cout << "Desired temperature: " <<desired_max<<endl;
+  if(*max==*min){
+    cout << "Desired temperature: " <<*max<<endl;
   }
   else{
-    cout << "Next smallest temperature: " <<desired_min << endl;
-    cout << "Next largest temperature: " <<desired_max << endl;
+    cout << "Next smallest temperature: " <<*min << endl;
+    cout << "Next largest temperature: " <<*max << endl;
   }
 return 0;
 }
