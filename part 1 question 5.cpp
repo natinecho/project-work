8c664b031acce62b082d917a8f3e5e9a897beecb
Olman <122857604+simera7@users.noreply.github.com>
2023-04-09 18:23:44 +0300

    Update part 1 question 5.cpp
    i have added pointer array

--- part 1 question 5.cpp
+++ part 1 question 5.cpp
@@ -3,7 +3,8 @@ using namespace std;
 int main() {
 
  //declare variables 
-  double loan,interest,payment,balance_loan[40],monthly_interest[40],count(0);
+  double loan,interest,payment;
+ double balance_loan[40],monthly_interest[40],count(0);
   cout<<"enter the loan amount:"; //prompt the user to enter loan amount
   cin>>loan;
   cout<<"enter interest rate:"; //get interest rate from the user
@@ -15,16 +16,16 @@ int main() {
   //loop until loan is fully paid off
   while(loan>0){
     // Calculate monthly interest and balance of loan
-      monthly_interest[i]=(loan*interest*0.01)/12;
-      balance_loan[i]=(loan-(payment-monthly_interest[i]));
+      *(monthly_interest+i)=(loan*interest*0.01)/12;
+      *(balance_loan+i)=(loan-(payment-monthly_interest[i]));
       // Update loan and payment if balance_loan is negative 
-      loan=balance_loan[i];
-      if(balance_loan[i]<0){
-         payment+=balance_loan[i];
-        balance_loan[i]=0;
+      loan=*(balance_loan+i);
+      if(*(balance_loan+i)<0){
+         payment+=*(balance_loan+i);
+        *(balance_loan+i)=0;
       }
       // Display monthly data in table format
-       cout<<'\n'<<"month"<<i+1<<'\t'<<'\t'<<monthly_interest[i]<<'\t'<<'\t'<<balance_loan[i]<<'\t'<<'\t'<<payment<<endl;
+       cout<<'\n'<<"month"<<i+1<<'\t'<<'\t'<<*(monthly_interest+i)<<'\t'<<'\t'<<*(balance_loan+i)<<'\t'<<'\t'<<payment<<endl;
       // Increment month count
       i++;
   }
