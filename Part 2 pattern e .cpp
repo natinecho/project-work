dcc71a7f95f350535aae0853340ce79699210534
RuthTadesse <125727910+RuthTadesse@users.noreply.github.com>
2023-04-09 18:05:22 +0300

    Update Part 2 pattern e .cpp

--- Part 2 pattern e .cpp
+++ Part 2 pattern e .cpp
@@ -47,3 +47,46 @@ int main(){
 
     return 0;
 }
+//code that accept number of row from user
+
+#include<iostream>
+using namespace std;
+int main()
+{
+int i,j,k,n;
+cout<<"Enter no. of rows: ";
+cin>>n;
+n=n/2;
+for(i=1;i<=n;i++)
+{
+for(j=1;j<=n-i;j++)
+cout<<" * ";
+for(k=1;k<=2*i-1;k++)
+{
+if(k==1 || k==2*i-1)
+cout<<" * ";
+else
+cout<<"   ";
+}
+for(j=1;j<=n-i;j++)
+cout<<" * ";
+cout<<"\n";
+}
+for(i=n;i>=1;i--)
+{
+for(j=1;j<=n-i;j++)
+cout<<" * ";
+for(k=1;k<=2*i-1;k++)
+{
+if(k==1 || k==2*i-1)
+cout<<" * ";
+else
+cout<<"   ";
+}
+for(j=1;j<=n-i;j++)
+cout<<" * ";
+cout<<"\n";
+}
+return 0; 
+}
+
